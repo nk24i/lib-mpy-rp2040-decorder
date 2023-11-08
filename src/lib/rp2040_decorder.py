aae92a0bd2868b43d3eed741452744680ac4f4e4
CMScom nk24 <cmscomnk24@CMScoms-MacBook-Pro.local>
2023-11-08 22:32:17 +0900

    add 'Receiver' class as main in library

---
+++ src/lib/rp2040_decorder.py
@@ -0,0 +1,12 @@
+
+class Receiver:
+    def __init__(self, pin_id, address):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    receiver = Receiver(0, 3)
+    receiver.run()
