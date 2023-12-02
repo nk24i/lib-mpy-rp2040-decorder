19498a144d34e3dc3ac1270e8b5a6d3c074548ed
CMScom nk24 <cmscomnk24@CMScoms-MacBook-Pro.local>
2023-12-02 15:55:42 +0900

    Update asm-pio code

--- src/lib/rp2040_decorder.py
+++ src/lib/rp2040_decorder.py
@@ -7,10 +7,22 @@ from struct import unpack
     set_init=rp2.PIO.IN_LOW,
     fifo_join=rp2.PIO.JOIN_RX,
     in_shiftdir=rp2.PIO.SHIFT_RIGHT,
+    # in_shiftdir=rp2.PIO.SHIFT_LEFT,
     autopush=True,
+    # push_thresh=16,  # 32bit(8bit*4, per FIFO)*8 -> 32pulse
 )
 def measure_pulse_length():
     # Measure pulse length by RP2040 programmable I/O
+    # wrap_target()
+    # set(x, 15)
+    # in_(x, 8)
+    # set(x, 14)
+    # in_(x, 8)
+    # set(x, 1)
+    # in_(x, 8)
+    # set(x, 0)
+    # in_(x, 8)
+    # wrap()
     set(pindirs, 0)
     wrap_target()
     set(x, 31)
@@ -96,7 +108,9 @@ class Receiver:
             in_base=pin_obj,
             jmp_pin=pin_obj,
             set_base=pin_obj,
-            push_thresh=256,  # 32bit(8bit*4, per FIFO)*8 -> 32pulse
+            # push_thresh=256,  # 32bit(8bit*4, per FIFO)*8 -> 32pulse
+            # push_thresh=32,  # 32bit(8bit*4, per FIFO)*8 -> 32pulse
+            # push_thresh=8,  # 32bit(8bit*4, per FIFO)*8 -> 32pulse
         )
         self.on_captcha = False  # if False waiting valid preamble
         self.preamble = Preamble()
@@ -107,20 +121,32 @@ class Receiver:
     def iter_event(self):
         # Yields pulse length
         while True:
-            counts = self.sm.get().to_bytes(4, "little")  # 20us 程度
-            count_array = unpack("<BBBB", counts)
-            # print(counts, count_array)
+            remain = self.sm.rx_fifo()
+            counts = self.sm.get()
+            counts2 = self.sm.get()
+            # counts3 = self.sm.get()
+            count_array = counts.to_bytes(4, "little")
+            count_array2 = counts2.to_bytes(4, "little")
+            # count_array3 = counts3.to_bytes(4, "little")
+            # print(remain)
+            # counts = self.sm.get().to_bytes(4, "little")  # 20us 程度
+            # count_array = unpack("<B", counts)
+            print(count_array, count_array2, remain)
+            count_array += count_array2
+            # count_array += count_array3
             for count in count_array:
+                # print(count)
                 yield count
 
     def count_to_captcha(self, count):
         signal_level = 1 if count > 13 and count < 16 else 0
+        print(signal_level, end="")
         if self.on_captcha:
             packet = self.captcha(signal_level)
             if packet is not None:
                 self.on_captcha = False
                 self.captcha.clear()
-                print(bin(packet), packet)
+                # print(bin(packet), packet)
                 # TODO: packet to command
         else:
             is_got_preamble = self.preamble(signal_level)
@@ -135,7 +161,8 @@ class Receiver:
     def run(self):
         self.sm.active(1)
         for count in self.iter_event():
-            self.count_to_captcha(count)
+            pass
+            # self.count_to_captcha(count)
 
 
 class Control():
